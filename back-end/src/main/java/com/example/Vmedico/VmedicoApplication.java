772c7827ab6344225fd912ec04a5d3e1c43cb52b
rama1832nathan <ramank1832@gmail.com>
2025-09-18 21:49:10 +0530

    initial

---
+++ back-end/src/main/java/com/example/Vmedico/VmedicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vmedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmedicoApplication.class, args);
+	}
+
+}
