9558dd9effddbcdf028778efc39e5a45ddbb686e
Shrivani15 <shrivani.15@gmail.com>
2025-11-03 01:17:12 +0530

    Added Lab portal

--- back-end/vmmedico/src/main/java/com/vmmedico/authentication/configuration/SecurityConfig.java
+++ back-end/vmmedico/src/main/java/com/vmmedico/authentication/configuration/SecurityConfig.java
@@ -24,11 +24,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/register/**", "/api/login", "/api/forgot-password","/api/change-password").permitAll()
-                        .requestMatchers("/api/patient/**","/api/appointments/book").hasAuthority("PATIENT")
+                        .requestMatchers("/api/patient/**","/api/appointments/book","/api/lab-bookings/book","/api/reports/**").hasAuthority("PATIENT")
                         .requestMatchers("/api/doctor/**").hasAuthority("DOCTOR")
                         .requestMatchers("/api/prescriptions/**").hasAuthority("DOCTOR")
                         .requestMatchers("/api/hospital/**","/api/hospital/add-doctor").hasAuthority("HOSPITAL_ADMIN")
-                        .requestMatchers("/api/lab/**").hasAuthority("LAB")
+                        .requestMatchers("/api/lab/**","/api/lab/add-tests","/api/lab/test/{id}","/api/lab/bookings/all").hasAuthority("LAB")
                         .requestMatchers("/api/profile/**").authenticated()
                         .anyRequest().authenticated()
                 )
